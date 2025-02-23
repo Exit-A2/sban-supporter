258ae150202efae63f1af1049e267b528b8d09b1
A2 <shakeikura132@gmail.com>
2025-02-23 14:09:47 +0900

    sbansupporterをパッケージ化するように方向転換

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# sban_supporter
+# SBAN Supporter
+模倣に役立つツール集です。
+## ツール一覧
+- base_to_midi<br>
+  数値系の暗号をMIDIに変換します。
+- 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sbansupporter",  # パッケージ名（pip listで表示される）
+    version="1.0",  # バージョン
+    description="模倣に使えるツール集",  # 説明
+    author="A2",  # 作者名
+    packages=find_packages(),  # 使うモジュール一覧を指定する
+    license="MIT",  # ライセンス
+)
